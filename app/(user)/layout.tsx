a0d284bc5db46e9e56d6a68670cdcd9357711a0a
Hoang Nguyen <74847239+Bibo1604@users.noreply.github.com>
2023-12-22 00:13:13 -0700

    Implement header

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../globals.css'
+import Header from '@/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {/* Header */}
-        
+        <Header />
 
         {children}
       </body>
