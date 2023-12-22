f1c335692eb6e38760a37e50570eaacc1d5f50bf
Hoang Nguyen <74847239+Bibo1604@users.noreply.github.com>
2023-12-21 22:32:09 -0700

    Customize studio page

---
+++ app/lib/sanity.ts
@@ -0,0 +1,15 @@
+import { createClient } from "next-sanity";
+import imageUrlBuilder from '@sanity/image-url'
+
+export const client = createClient({
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+    useCdn: true,
+});
+
+const builder = imageUrlBuilder(client)
+
+export function urlFor(source: any) {
+    return builder.image(source);
+}
--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+import StudioNavbar from './components/StudioNavbar';
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
@@ -9,13 +10,15 @@ const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 export default defineConfig({
   name: 'Personal_Blog',
   title: 'Personal Blog',
-
   projectId,
   dataset,
-
   plugins: [deskTool(), visionTool()],
-
   schema: {
     types: schemaTypes,
   },
+  studio: {
+    components: {
+      navbar: StudioNavbar
+    }
+  }
 })
